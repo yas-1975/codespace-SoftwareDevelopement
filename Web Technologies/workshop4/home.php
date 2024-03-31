692170baaee30e28d8cbd0d6965c259a114f3e0e
Yasemin Hasip <y-hasip@outlook.com>
2024-03-31 14:34:37 +0100

    loaded register file

--- Web Technologies/workshop4/home.php
+++ Web Technologies/workshop4/home.php
@@ -22,22 +22,29 @@ if ( mysqli_num_rows( $r ) > 0 )
     while ( $row = mysqli_fetch_array( $r, MYSQLI_ASSOC ))
     {
         echo '
-        <div class="col-md-3 d-flex justify-content-center">
-            <div class="card" style="width: 18rem;">
-                <img src="'. $row['item_img'].'" class="card-img-top" alt="'. $row['product_name'].'">
-                <div class="card-body text-center">
-                    <h5 class="card-title">'. $row['product_name'].'</h5>
-                    <p class="card-text">'. $row['item_desc'].'</p>
-                </div>
+	
+		<div class="col-md-3 d-flex justify-content-center">
+			<div class="card" style="width: 20rem;">
+				<div class="card-body text-center">
+					<img src="'. $row['item_img'].'" class="card-img-top" alt="'. $row['product_name'].'">
+				</div>
+				<div class="card-footer bg-transparent border-white text-center">
+					<h5 class="card-title">'. $row['product_name'].'</h5>		
+					<p class="card-text">'. $row['item_desc'].'</p>
+				</div>
                 <div class="card-footer bg-transparent border-dark text-center">
                     <p class="card-text">&pound '. $row['item_price'].'</p>
                 </div>
                 <div class="card-footer text-muted">
                     <a href="added.php?item_id='.$row['item_id'].'" class="btn btn-light btn-block">Add to Cart</a>
                 </div>
-            </div>
-        </div>';  
-    }
+				
+			</div>
+		</div>
+		';
+	}	
+
+	
     # Close database connection.
     mysqli_close( $link);
 }
